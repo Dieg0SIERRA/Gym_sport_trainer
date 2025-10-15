94d86fa3e588e5f891da129086bb010ced0dfa9c
DiegoSIERRA <jhondiego.00@gmail.com>
2025-10-15 18:48:59 +0200

    Adding implementation of database in main file

--- main.cpp
+++ main.cpp
@@ -1,14 +1,26 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQmlContext>
+#include "database/DatabaseManager.h"
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
+    
+    // Create DatabaseManager instance
+    DatabaseManager dbManager;    
 
+    // Create and configure the QML engine
     QQmlApplicationEngine engine;
+    
+    // Expose DatabaseManager to the QML context
+    engine.rootContext()->setContextProperty("DatabaseManager", &dbManager);
+    
+    // Cargar el archivo QML principal
     const QUrl url(QStringLiteral("qrc:/GymTracker/Main.qml"));
+
     QObject::connect(
-        &engine,
+    	&engine,
         &QQmlApplicationEngine::objectCreated,
         &app,
         [url](QObject *obj, const QUrl &objUrl) {
@@ -16,6 +28,7 @@ int main(int argc, char *argv[])
                 QCoreApplication::exit(-1);
         },
         Qt::QueuedConnection);
+
     engine.load(url);
 
     return app.exec();
